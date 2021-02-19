9a469a23340d9814f9970a1b00ab0a570df1aab5
zhamadeh <zeid.hamadeh@hotmail.com>
2021-02-19 10:27:41 -0800

    push master

---
+++ master.R
@@ -0,0 +1,26 @@
+
+suppressMessages(suppressPackageStartupMessages(suppressWarnings(require(GenomicRanges))))
+suppressMessages(suppressPackageStartupMessages(suppressWarnings(require(GenomicAlignments))))
+suppressMessages(suppressPackageStartupMessages(suppressWarnings(require(DNAcopy))))
+suppressMessages(suppressPackageStartupMessages(suppressWarnings(require(doParallel))))
+suppressMessages(suppressPackageStartupMessages(suppressWarnings(require(tiydyverse))))
+suppressMessages(suppressPackageStartupMessages(suppressWarnings(require(ggplot2))))
+suppressMessages(suppressPackageStartupMessages(suppressWarnings(require(cowplot))))
+suppressMessages(suppressPackageStartupMessages(suppressWarnings(require(caTools))))
+suppressMessages(suppressPackageStartupMessages(suppressWarnings(require(gplots))))
+suppressMessages(suppressPackageStartupMessages(suppressWarnings(require(ReorderCluster))))
+suppressMessages(suppressPackageStartupMessages(suppressWarnings(require(BiocManager))))
+suppressMessages(suppressPackageStartupMessages(suppressWarnings(BiocManager::install("AneuFinder"))))
+suppressMessages(suppressPackageStartupMessages(suppressWarnings(require(AneuFinder))))
+suppressMessages(suppressPackageStartupMessages(suppressWarnings(require(BSgenome.Hsapiens.UCSC.hg19))))
+
+Aneufinder(inputfolder='../NEW_PAPER_OLD_DATA/se_alignment_pipeline/mdup/', outputfolder='aneufinder_output_varWidthRef-Niek/',
+           numCPU=13, method=c('dnacopy','HMM'),
+           configfile=NULL, reuse.existing.files=TRUE, binsizes=1e5, stepsizes=1e5, variable.width.reference="../../../projects/lansdorp/analysis/variable_width_ref_hg002_merged.bam", 
+           reads.per.bin=NULL, pairedEndReads=FALSE,remove.duplicate.reads=TRUE, min.mapq=10, 
+           use.bamsignals=FALSE, reads.store=FALSE, 
+           #correction.method="GC", GC.BSgenome = BSgenome.Hsapiens.UCSC.hg19, 
+           strandseq=TRUE, 
+           R=10, sig.lvl=0.1, eps=0.01, max.time=60, max.iter=5000, num.trials=15, states=c('zero-inflation',paste0(0:10,'-somy')), 
+           confint=NULL, refine.breakpoints=FALSE, hotspot.bandwidth=NULL, hotspot.pval=5e-2, cluster.plots=TRUE)
+
